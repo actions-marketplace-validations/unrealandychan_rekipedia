3728763ca490210d4eca5d7268f8f9d64cd5ea7e
unrealandychan <unrealandychan@users.noreply.github.com>
2026-05-11 03:08:29 +0000

    fix: update test_version_still_present to 0.14.0

--- tests/test_api.py
+++ tests/test_api.py
@@ -313,4 +313,4 @@ class TestTopLevelImport:
     def test_version_still_present(self):
         import rekipedia
 
-        assert rekipedia.__version__ == "0.13.0"
+        assert rekipedia.__version__ == "0.14.0"
